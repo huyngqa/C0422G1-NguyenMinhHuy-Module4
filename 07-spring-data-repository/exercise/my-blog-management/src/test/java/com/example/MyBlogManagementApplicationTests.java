b0391a5e8d07fd47e7d8e20750ca3f946829f676
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-08-23 16:39:05 +0700

    ss6

---
+++ 07-spring-data-repository/exercise/my-blog-management/src/test/java/com/example/MyBlogManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyBlogManagementApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
