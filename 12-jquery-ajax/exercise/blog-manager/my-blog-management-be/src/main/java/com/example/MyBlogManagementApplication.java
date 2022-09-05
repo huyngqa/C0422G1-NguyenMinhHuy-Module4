5ddb479505f4416ab8df643b6637cf1d44368bd3
huyngqa <104546437+huyngqa@users.noreply.github.com>
2022-09-05 16:03:06 +0700

    ss12

---
+++ 12-jquery-ajax/exercise/blog-manager/my-blog-management-be/src/main/java/com/example/MyBlogManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyBlogManagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyBlogManagementApplication.class, args);
+    }
+
+}
